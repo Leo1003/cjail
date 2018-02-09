68f6695ea7fd8fed8299cad9ee9ff21cc8135939
Leo Chen <leo881003@gmail.com>
2018-02-09 22:45:14 +0800

    completed cgroup utils

--- src/cgroup.c
+++ src/cgroup.c
@@ -2,13 +2,22 @@
 #include "cgroup.h"
 #include "utils.h"
 
+#include <fcntl.h>
 #include <linux/limits.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
 
 static char cgroup_root[PATH_MAX] = CGROUP_DEFAULT_ROOT;
+
+inline static void get_cgpath(char *cgpath, const char* subsystem)
+{
+    char subpath[PATH_MAX];
+    combine_path(subpath, cgroup_root, subsystem);
+    snprintf(cgpath, sizeof(char) * PATH_MAX, "%s/"CGROUP_NAME, subpath, getpid());
+}
 
 int cgroup_set_root(const char *path)
 {
@@ -25,9 +34,8 @@ int cgroup_set_root(const char *path)
 
 int cgroup_create(const char* subsystem)
 {
-    char subpath[PATH_MAX], cgpath[PATH_MAX];
-    combine_path(subpath, cgroup_root, subsystem);
-    snprintf(cgpath, sizeof(char) * PATH_MAX, "%s/"CGROUP_NAME, subpath, getpid());
+    char cgpath[PATH_MAX];
+    get_cgpath(cgpath, subsystem);
     IFERR(mkdir(cgpath, 0755))
         goto error;
     return 0;
@@ -40,3 +48,93 @@ int cgroup_create(const char* subsystem)
     return -1;
 }
 
+int cgroup_read(const char* subsystem, const char* name, const char* fmt, ...)
+{
+    va_list ap;
+    char cgpath[PATH_MAX], settingpath[PATH_MAX];
+    get_cgpath(cgpath, subsystem);
+    snprintf(settingpath, sizeof(char) * PATH_MAX, "%s/%s", cgpath, name);
+
+    FILE *fp = fopen(settingpath, "r");
+    if(!fp)
+        goto error;
+
+    va_start(ap, fmt);
+    IFERR(vfscanf(fp, fmt, ap))
+    {
+        int ferr;
+        if((ferr = ferror(fp)))
+            perrf("File I/O error: %s\n" , strerror(ferr));
+        va_end(ap);
+        fclose(fp);
+        goto error;
+    }
+    va_end(ap);
+    fclose(fp);
+    return 0;
+
+    error:
+    pdebugf("cgroup_read error: %s\n", settingpath);
+    PRINTERR("read cgroup");
+    return -1;
+}
+
+int cgroup_write(const char* subsystem, const char* name, const char* fmt, ...)
+{
+    va_list ap;
+    char cgpath[PATH_MAX], settingpath[PATH_MAX];
+    get_cgpath(cgpath, subsystem);
+    snprintf(settingpath, sizeof(char) * PATH_MAX, "%s/%s", cgpath, name);
+
+    FILE *fp = fopen(settingpath, "w");
+    if(!fp)
+        goto error;
+
+    va_start(ap, fmt);
+    IFERR(vfprintf(fp, fmt, ap))
+    {
+        int ferr;
+        if((ferr = ferror(fp)))
+            perrf("File I/O error: %s\n" , strerror(ferr));
+        va_end(ap);
+        fclose(fp);
+        goto error;
+    }
+    va_end(ap);
+    fclose(fp);
+    return 0;
+
+    error:
+    pdebugf("cgroup_write error: %s\n", settingpath);
+    PRINTERR("write cgroup");
+    return -1;
+}
+
+int cgroup_open_tasks(const char* subsystem)
+{
+    char cgpath[PATH_MAX], taskspath[PATH_MAX];
+    int fd;
+    get_cgpath(cgpath, subsystem);
+    snprintf(taskspath, sizeof(char) * PATH_MAX, "%s/tasks", cgpath);
+
+    IFERR((fd = open(taskspath, O_RDWR)))
+    {
+        pdebugf("cgroup_open_tasks error: %s\n", taskspath);
+        PRINTERR("open cgroup tasks");
+    }
+    return fd;
+}
+
+int cgroup_destory(const char* subsystem)
+{
+    char cgpath[PATH_MAX];
+    get_cgpath(cgpath, subsystem);
+
+    IFERR(rmdir(cgpath))
+    {
+        pdebugf("cgroup_destory error: %s\n", cgpath);
+        PRINTERR("destroy cgroup");
+        return -1;
+    }
+    return 0;
+}
