b58e491faea97cfc93fad84409d9c3af97ae5f86
Leo Chen <leo881003@gmail.com>
2018-05-20 17:05:45 +0800

    add a basic unit test

---
+++ test/basic_test.c
@@ -0,0 +1,49 @@
+#include "cjail.h"
+
+#include <check.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+START_TEST(basic_test_1)
+{
+    if (geteuid()) {
+        ck_abort_msg("Should be tested as root!");
+    }
+    char *argv[] = { "/bin/echo", NULL };
+    struct cjail_para para;
+    cjail_para_init(&para);
+    para.argv = argv;
+    ck_assert_int_eq(cjail_exec(&para, NULL), 0);
+}
+END_TEST
+
+Suite* suite()
+{
+    Suite *s = suite_create("basic");
+    TCase *t_basic = tcase_create("basic_test");
+
+    tcase_add_test(t_basic, basic_test_1);
+
+    suite_add_tcase(s, t_basic);
+    return s;
+}
+
+int main()
+{
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = suite();
+    sr = srunner_create(s);
+
+#ifdef NDEBUG
+    srunner_run_all(sr, CK_NORMAL);
+#else
+    srunner_run_all(sr, CK_VERBOSE);
+#endif
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
