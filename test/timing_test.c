e0cbcf05a3085ae5fba6afde25b82cc062b760a9
Leo <leo881003@gmail.com>
2018-11-14 01:12:55 +0800

    Changed unit tests framework to Criterion

--- test/timing_test.c
+++ test/timing_test.c
@@ -1,45 +1,54 @@
 #include "cjail.h"
 
-#include <check.h>
+#include <criterion/criterion.h>
+#include <criterion/assert.h>
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-START_TEST(timing_test_1)
+void setup() {
+    if (geteuid()) {
+        cr_skip_test("Not running as root!\n");
+    }
+}
+
+TestSuite(timing_test, .init = setup, .timeout = 12);
+
+Test(timing_test, test_1)
 {
     char *argv[] = { "/bin/sleep", "3", NULL };
     struct cjail_para para;
     struct cjail_result result;
     cjail_para_init(&para);
     para.argv = argv;
-    ck_assert_int_eq(cjail_exec(&para, &result), 0);
-    ck_assert_int_eq(result.info.si_code, CLD_EXITED);
-    ck_assert_int_eq(result.info.si_status, EXIT_SUCCESS);
-    ck_assert_int_eq(result.time.tv_sec, 3);
+    cr_assert_eq(cjail_exec(&para, &result), 0);
+    cr_expect_eq(result.info.si_code, CLD_EXITED);
+    cr_expect_eq(result.info.si_status, EXIT_SUCCESS);
+    cr_expect_eq(result.time.tv_sec, 3);
 }
-END_TEST
 
-START_TEST(timing_test_2)
+Test(timing_test, test_2)
 {
     srandom(time(NULL));
     int sec = (random() % 10 + 1);
     char sec_s[16];
     snprintf(sec_s, sizeof(sec_s), "%d", sec);
-    fprintf(stderr, "Random timing test: %d sec\n", sec);
+    cr_log_info("Random timing test: %d sec\n", sec);
     char *argv[] = { "/bin/sleep", sec_s, NULL };
     struct cjail_para para;
     struct cjail_result result;
     cjail_para_init(&para);
     para.argv = argv;
-    ck_assert_int_eq(cjail_exec(&para, &result), 0);
-    ck_assert_int_eq(result.info.si_code, CLD_EXITED);
-    ck_assert_int_eq(result.info.si_status, EXIT_SUCCESS);
-    ck_assert_int_eq(result.time.tv_sec, sec);
+    cr_assert_eq(cjail_exec(&para, &result), 0);
+    cr_expect_eq(result.info.si_code, CLD_EXITED);
+    cr_expect_eq(result.info.si_status, EXIT_SUCCESS);
+    cr_expect_eq(result.time.tv_sec, sec);
 }
-END_TEST
 
-START_TEST(timeout_test_1)
+TestSuite(timeout_test, .init = setup, .timeout = 10);
+
+Test(timeout_test, test_1)
 {
     char *argv[] = { "/bin/sleep", "3", NULL };
     struct cjail_para para;
@@ -47,53 +56,9 @@ START_TEST(timeout_test_1)
     cjail_para_init(&para);
     para.argv = argv;
     para.lim_time.tv_sec = 2;
-    ck_assert_int_eq(cjail_exec(&para, &result), 0);
-    ck_assert_int_eq(result.info.si_code, CLD_KILLED);
-    ck_assert_int_eq(result.info.si_status, SIGKILL);
-    ck_assert_int_eq(result.timekill, 1);
-    ck_assert_int_eq(result.time.tv_sec, 2);
+    cr_assert_eq(cjail_exec(&para, &result), 0);
+    cr_expect_eq(result.info.si_code, CLD_KILLED);
+    cr_expect_eq(result.info.si_status, SIGKILL);
+    cr_expect_eq(result.timekill, 1);
+    cr_expect_eq(result.time.tv_sec, 2);
 }
-END_TEST
-
-Suite* suite()
-{
-    Suite *s = suite_create("timing");
-    TCase *t_timing = tcase_create("timing_test");
-    TCase *t_timeout = tcase_create("timeout_test");
-    tcase_set_timeout(t_timing, 30);
-    tcase_set_timeout(t_timeout, 30);
-
-    tcase_add_test(t_timing, timing_test_1);
-    tcase_add_test(t_timing, timing_test_2);
-
-    tcase_add_test(t_timeout, timeout_test_1);
-
-    suite_add_tcase(s, t_timing);
-    suite_add_tcase(s, t_timeout);
-    return s;
-}
-
-int main()
-{
-    if (geteuid()) {
-        fprintf(stderr, "Should be tested as root!\n");
-        fprintf(stderr, "Skip this test.\n");
-        return EXIT_SUCCESS;
-    }
-    int number_failed;
-    Suite *s;
-    SRunner *sr;
-
-    s = suite();
-    sr = srunner_create(s);
-
-#ifdef NDEBUG
-    srunner_run_all(sr, CK_NORMAL);
-#else
-    srunner_run_all(sr, CK_VERBOSE);
-#endif
-    number_failed = srunner_ntests_failed(sr);
-    srunner_free(sr);
-    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
-}
-
--- test/utils_test.c
+++ test/utils_test.c
@@ -3,7 +3,8 @@
 #include "fds.h"
 #include "utils.h"
 
-#include <check.h>
+#include <criterion/criterion.h>
+#include <criterion/assert.h>
 #include <linux/limits.h>
 #include <sched.h>
 #include <stdio.h>
@@ -17,18 +18,17 @@
  *  Tests for testing cpuset_tostr()
  */
 
-START_TEST(test_cpuset_tostr_1)
+Test(test_cpuset_tostr, test_1)
 {
     char str[STR_LEN];
     cpu_set_t cpuset;
     CPU_ZERO(&cpuset);
     CPU_SET(0, &cpuset);
     cpuset_tostr(&cpuset, str, sizeof(str));
-    ck_assert_str_eq("0", str);
+    cr_assert_str_eq("0", str);
 }
-END_TEST
 
-START_TEST(test_cpuset_tostr_2)
+Test(test_cpuset_tostr, test_2)
 {
     char str[STR_LEN];
     cpu_set_t cpuset;
@@ -36,11 +36,10 @@ START_TEST(test_cpuset_tostr_2)
     CPU_SET(0, &cpuset);
     CPU_SET(1, &cpuset);
     cpuset_tostr(&cpuset, str, sizeof(str));
-    ck_assert_str_eq("0-1", str);
+    cr_assert_str_eq("0-1", str);
 }
-END_TEST
 
-START_TEST(test_cpuset_tostr_3)
+Test(test_cpuset_tostr, test_3)
 {
     char str[STR_LEN];
     cpu_set_t cpuset;
@@ -53,11 +52,10 @@ START_TEST(test_cpuset_tostr_3)
     CPU_SET(9, &cpuset);
     CPU_SET(11, &cpuset);
     cpuset_tostr(&cpuset, str, sizeof(str));
-    ck_assert_str_eq("0-2,4-5,9,11", str);
+    cr_assert_str_eq("0-2,4-5,9,11", str);
 }
-END_TEST
 
-START_TEST(test_cpuset_tostr_4)
+Test(test_cpuset_tostr, test_4)
 {
     char str[STR_LEN];
     cpu_set_t cpuset;
@@ -68,11 +66,10 @@ START_TEST(test_cpuset_tostr_4)
     CPU_SET(6, &cpuset);
     CPU_SET(9, &cpuset);
     cpuset_tostr(&cpuset, str, sizeof(str));
-    ck_assert_str_eq("1,4-6,9", str);
+    cr_assert_str_eq("1,4-6,9", str);
 }
-END_TEST
 
-START_TEST(test_cpuset_tostr_5)
+Test(test_cpuset_tostr, test_5)
 {
     char str[STR_LEN];
     cpu_set_t cpuset;
@@ -83,11 +80,10 @@ START_TEST(test_cpuset_tostr_5)
     CPU_SET(7, &cpuset);
     CPU_SET(9, &cpuset);
     cpuset_tostr(&cpuset, str, sizeof(str));
-    ck_assert_str_eq("1,3,5,7,9", str);
+    cr_assert_str_eq("1,3,5,7,9", str);
 }
-END_TEST
 
-START_TEST(test_cpuset_tostr_6)
+Test(test_cpuset_tostr, test_6)
 {
     char str[10];
     cpu_set_t cpuset;
@@ -98,17 +94,15 @@ START_TEST(test_cpuset_tostr_6)
     CPU_SET(7, &cpuset);
     CPU_SET(11, &cpuset);
     int ret = cpuset_tostr(&cpuset, str, sizeof(str));
-    ck_assert_int_eq(-1, ret);
-    ck_assert_str_eq("1,3,5,7,1", str);
+    cr_assert_eq(-1, ret);
+    cr_assert_str_eq("1,3,5,7,1", str);
 }
-END_TEST
-
 
 /*
  *  Tests for testing combine_path()
  */
 
-START_TEST(test_cpuset_parse_1)
+Test(test_cpuset_parse, test_1)
 {
     cpu_set_t cpu1, cpu2;
     CPU_ZERO(&cpu1);
@@ -119,12 +113,11 @@ START_TEST(test_cpuset_parse_1)
     CPU_SET(5, &cpu1);
     CPU_SET(7, &cpu1);
     CPU_SET(11, &cpu1);
-    ck_assert_int_eq(cpuset_parse("0,1,3-5,7,11", &cpu2), 0);
-    ck_assert_int_ne(CPU_EQUAL(&cpu1, &cpu2), 0);
+    cr_assert_eq(cpuset_parse("0,1,3-5,7,11", &cpu2), 0);
+    cr_assert_neq(CPU_EQUAL(&cpu1, &cpu2), 0);
 }
-END_TEST
 
-START_TEST(test_cpuset_parse_2)
+Test(test_cpuset_parse, test_2)
 {
     cpu_set_t cpu1, cpu2;
     CPU_ZERO(&cpu1);
@@ -135,11 +128,10 @@ START_TEST(test_cpuset_parse_2)
     CPU_SET(5, &cpu1);
     CPU_SET(7, &cpu1);
     CPU_SET(11, &cpu1);
-    ck_assert_int_eq(cpuset_parse("0 ,1,3-5,7,11", &cpu2), -1);
+    cr_assert_eq(cpuset_parse("0 ,1,3-5,7,11", &cpu2), -1);
 }
-END_TEST
 
-START_TEST(test_cpuset_parse_3)
+Test(test_cpuset_parse, test_3)
 {
     cpu_set_t cpu1, cpu2;
     CPU_ZERO(&cpu1);
@@ -150,56 +142,50 @@ START_TEST(test_cpuset_parse_3)
     CPU_SET(5, &cpu1);
     CPU_SET(6, &cpu1);
     CPU_SET(11, &cpu1);
-    ck_assert_int_eq(cpuset_parse("1-6,11", &cpu2), 0);
-    ck_assert_int_ne(CPU_EQUAL(&cpu1, &cpu2), 0);
+    cr_assert_eq(cpuset_parse("1-6,11", &cpu2), 0);
+    cr_assert_neq(CPU_EQUAL(&cpu1, &cpu2), 0);
 }
-END_TEST
 
 /*
  *  Tests for testing combine_path()
  */
 
-START_TEST(test_combine_path_1)
+Test(test_combine_path, test_1)
 {
     char str[PATH_MAX];
     combine_path(str, "/root", "/dev");
-    ck_assert_str_eq("/root/dev", str);
+    cr_assert_str_eq("/root/dev", str);
 }
-END_TEST
 
-START_TEST(test_combine_path_2)
+Test(test_combine_path, test_2)
 {
     char str[PATH_MAX];
     combine_path(str, "/root", "dev");
-    ck_assert_str_eq("/root/dev", str);
+    cr_assert_str_eq("/root/dev", str);
 }
-END_TEST
 
-START_TEST(test_combine_path_3)
+Test(test_combine_path, test_3)
 {
     char str[PATH_MAX];
     combine_path(str, "/", "/dev");
-    ck_assert_str_eq("/dev", str);
+    cr_assert_str_eq("/dev", str);
 }
-END_TEST
 
-START_TEST(test_combine_path_4)
+Test(test_combine_path, test_4)
 {
     char str[PATH_MAX];
     combine_path(str, NULL, "/dev");
-    ck_assert_str_eq("/dev", str);
+    cr_assert_str_eq("/dev", str);
 }
-END_TEST
 
-START_TEST(test_combine_path_5)
+Test(test_combine_path, test_5)
 {
     char str[PATH_MAX];
     combine_path(str, NULL, "dev");
-    ck_assert_str_eq("/dev", str);
+    cr_assert_str_eq("/dev", str);
 }
-END_TEST
 
-START_TEST(test_combine_path_6)
+Test(test_combine_path, test_6)
 {
     char str[PATH_MAX];
     char root[PATH_MAX], path[PATH_MAX], ans[PATH_MAX];
@@ -222,132 +208,58 @@ START_TEST(test_combine_path_6)
     ans[PATH_MAX - 1] = '\0';
 
     combine_path(str, root, path);
-    ck_assert_str_eq(ans, str);
+    cr_assert_str_eq(ans, str);
 }
-END_TEST
 
-START_TEST(test_combine_path_7)
+Test(test_combine_path, test_7)
 {
     char str[PATH_MAX];
     combine_path(str, "", "dev");
-    ck_assert_str_eq("/dev", str);
+    cr_assert_str_eq("/dev", str);
 }
-END_TEST
 
-START_TEST(test_combine_path_8)
+Test(test_combine_path, test_8)
 {
     char str[PATH_MAX];
     combine_path(str, "", "");
-    ck_assert_str_eq("/", str);
+    cr_assert_str_eq("/", str);
 }
-END_TEST
 
-START_TEST(test_combine_path_9)
+Test(test_combine_path, test_9)
 {
     char str[PATH_MAX];
     combine_path(str, "/new_root", "");
-    ck_assert_str_eq("/new_root/", str);
+    cr_assert_str_eq("/new_root/", str);
 }
-END_TEST
 
 /*
  *  Tests for testing strrmchr()
  */
 
-START_TEST(test_strrmchr_1)
+Test(test_strrmchr, test_1)
 {
     char str[STR_LEN] = "plogdhgjkahjgdfga";
     strrmchr(str, 2);
-    ck_assert_str_eq("plgdhgjkahjgdfga", str);
+    cr_assert_str_eq("plgdhgjkahjgdfga", str);
 }
-END_TEST
 
-START_TEST(test_strrmchr_2)
+Test(test_strrmchr, test_2)
 {
     char str[STR_LEN] = "plogdhgjkahjgdfga";
     strrmchr(str, -5);
-    ck_assert_str_eq("plogdhgjkahjdfga", str);
+    cr_assert_str_eq("plogdhgjkahjdfga", str);
 }
-END_TEST
 
-START_TEST(test_strrmchr_3)
+Test(test_strrmchr, test_3)
 {
     char str[STR_LEN] = "pl";
     int ret = strrmchr(str, 2);
-    ck_assert_int_eq(-1, ret);
+    cr_assert_eq(-1, ret);
 }
-END_TEST
 
-START_TEST(test_strrmchr_4)
+Test(test_strrmchr, test_4)
 {
     char str[STR_LEN] = "pl";
     int ret = strrmchr(str, -3);
-    ck_assert_int_eq(-1, ret);
-}
-END_TEST
-
-/*
- *  libcheck suite setup
- */
-
-Suite* suite_utils()
-{
-    Suite *s;
-    TCase *t_cputostr, *t_cpuparse, *t_combine, *t_strrmchr;
-    s = suite_create("utils");
-
-    t_cputostr = tcase_create("cpuset_tostr");
-    tcase_add_test(t_cputostr, test_cpuset_tostr_1);
-    tcase_add_test(t_cputostr, test_cpuset_tostr_2);
-    tcase_add_test(t_cputostr, test_cpuset_tostr_3);
-    tcase_add_test(t_cputostr, test_cpuset_tostr_4);
-    tcase_add_test(t_cputostr, test_cpuset_tostr_5);
-    tcase_add_test(t_cputostr, test_cpuset_tostr_6);
-
-    t_cpuparse = tcase_create("cpuset_parse");
-    tcase_add_test(t_cpuparse, test_cpuset_parse_1);
-    tcase_add_test(t_cpuparse, test_cpuset_parse_2);
-    tcase_add_test(t_cpuparse, test_cpuset_parse_3);
-
-    t_combine = tcase_create("combine_path");
-    tcase_add_test(t_combine, test_combine_path_1);
-    tcase_add_test(t_combine, test_combine_path_2);
-    tcase_add_test(t_combine, test_combine_path_3);
-    tcase_add_test(t_combine, test_combine_path_4);
-    tcase_add_test(t_combine, test_combine_path_5);
-    tcase_add_test(t_combine, test_combine_path_6);
-    tcase_add_test(t_combine, test_combine_path_7);
-    tcase_add_test(t_combine, test_combine_path_8);
-    tcase_add_test(t_combine, test_combine_path_9);
-
-    t_strrmchr = tcase_create("strrmchr");
-    tcase_add_test(t_strrmchr, test_strrmchr_1);
-    tcase_add_test(t_strrmchr, test_strrmchr_2);
-    tcase_add_test(t_strrmchr, test_strrmchr_3);
-    tcase_add_test(t_strrmchr, test_strrmchr_4);
-
-    suite_add_tcase(s, t_cputostr);
-    suite_add_tcase(s, t_cpuparse);
-    suite_add_tcase(s, t_combine);
-    suite_add_tcase(s, t_strrmchr);
-    return s;
-}
-
-int main(int argc, char *argv[])
-{
-    int number_failed;
-    Suite *s;
-    SRunner *sr;
-
-    s = suite_utils();
-    sr = srunner_create(s);
-
-#ifdef NDEBUG
-    srunner_run_all(sr, CK_NORMAL);
-#else
-    srunner_run_all(sr, CK_VERBOSE);
-#endif
-    number_failed = srunner_ntests_failed(sr);
-    srunner_free(sr);
-    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+    cr_assert_eq(-1, ret);
 }
