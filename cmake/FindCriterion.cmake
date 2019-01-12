65197192ee2d1723447a4d2cc49e2a4cb5a8fb38
Leo <leo881003@gmail.com>
2019-01-12 22:14:20 +0800

    Update FindCriterion

--- cmake/FindCriterion.cmake
+++ cmake/FindCriterion.cmake
@@ -7,21 +7,42 @@
 #  CRITERION_FOUND - System has Criterion
 #  CRITERION_INCLUDE_DIRS - The Criterion include directories
 #  CRITERION_LIBRARIES - The libraries needed to use Criterion
+#  CRITERION_VERSION - The version of Criterion
+#
+# and the following imported target:
+#   Criterion::Criterion - The Criterion framework
 
 find_package(PkgConfig)
+pkg_check_modules(PC_CRITERION criterion)
 
-find_path(CRITERION_INCLUDE_DIR criterion/criterion.h
-          PATH_SUFFIXES criterion)
+find_path(CRITERION_INCLUDE_DIRS
+    NAMES criterion/criterion.h
+    HINTS ${PC_CRITERION_INCLUDE_DIRS}
+        ${PC_CRITERION_INCLUDEDIR}
+    PATH_SUFFIXES criterion
+)
 
-find_library(CRITERION_LIBRARY NAMES criterion libcriterion)
+find_library(CRITERION_LIBRARIES
+    NAMES criterion libcriterion
+    HINTS ${PC_CRITERION_LIBRARY_DIRS}
+        ${PC_CRITERION_LIBDIR}
+)
 
-set(CRITERION_LIBRARIES ${CRITERION_LIBRARY})
-set(CRITERION_INCLUDE_DIRS ${CRITERION_INCLUDE_DIR})
+set(CRITERION_VERSION ${PC_CRITERION_VERSION})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIET and REQUIRED arguments and set CRITERION_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(Criterion DEFAULT_MSG
-                                  CRITERION_LIBRARY CRITERION_INCLUDE_DIR)
+find_package_handle_standard_args(Criterion
+    FOUND_VAR CRITERION_FOUND
+    REQUIRED_VARS CRITERION_LIBRARIES CRITERION_INCLUDE_DIRS
+    VERSION_VAR LIBSECCOMP_VERSION
+)
 
-mark_as_advanced(CRITERION_INCLUDE_DIR CRITERION_LIBRARY)
+if(CRITERION_FOUND AND NOT TARGET Criterion::Criterion)
+    add_library(Criterion::Criterion INTERFACE IMPORTED)
+    set_target_properties(Criterion::Criterion PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES   ${CRITERION_INCLUDE_DIRS}
+        INTERFACE_LINK_LIBRARIES        ${CRITERION_LIBRARIES}
+    )
+endif()
